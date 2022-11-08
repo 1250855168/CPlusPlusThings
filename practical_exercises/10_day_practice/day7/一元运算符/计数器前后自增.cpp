039631343d742045fcdaadee571f3344cb0fb995
杨春华 <1250855168@qq.com>
2022-11-08 08:59:24 +0800

    Update 计数器前后自增.cpp
    #include<iostream>
    
    using namespace std;
    
    class Counter {
    private:
        int n;
    public:
        Counter(int i = 0) : n(i) {};
    
        Counter operator++();
    
        Counter operator++(int);
    
        friend Counter operator--(Counter &c);
    
         Counter operator--(int);
    
        void display();
    };
    
    Counter Counter::operator++() {
        ++n;
        return *this;
    }
    
    Counter Counter::operator++(int) {
        Counter t = *this;
        n++;
        return t;
    }
    
    Counter operator--(Counter &c) {
        --c.n;
        return c;
    }
    
    Counter Counter::operator--(int) {
        Counter t = *this;
        this->n--;
        return t;
    }
    
    void Counter::display() {
        cout << "counter number=" << n << endl;
    }
    
    int main(int argc, char const *argv[]) {
        Counter a;
        ++a;
        a.display();
        a++;
        a.display();
        --a;
        a.display();
        a--;
        a.display();
        return 0;
    }
    }

--- practical_exercises/10_day_practice/day7/一元运算符/计数器前后自增.cpp
+++ practical_exercises/10_day_practice/day7/一元运算符/计数器前后自增.cpp
@@ -1,40 +1,51 @@
-//设计一个计数器counter，用类成员重载自增运算符实现计数器的自增，用友元重载实现计数器的自减。
 #include<iostream>
+
 using namespace std;
-class Counter{
-    private:
-        int n;
-    public:
-        Counter(int i=0):n(i){};
-        Counter operator++();
-        Counter operator++(int);
-        friend Counter operator--(Counter &c);
-        friend Counter operator--(Counter &c,int);
-        void display();
+
+class Counter {
+private:
+    int n;
+public:
+    Counter(int i = 0) : n(i) {};
+
+    Counter operator++();
+
+    Counter operator++(int);
+
+    friend Counter operator--(Counter &c);
+
+     Counter operator--(int);
+
+    void display();
 };
-Counter Counter::operator++(){
+
+Counter Counter::operator++() {
     ++n;
     return *this;
 }
-Counter Counter::operator++(int){
-    Counter t=*this;
+
+Counter Counter::operator++(int) {
+    Counter t = *this;
     n++;
     return t;
 }
-Counter operator--(Counter &c){
+
+Counter operator--(Counter &c) {
     --c.n;
     return c;
 }
-Counter operator--(Counter &c,int){
-    Counter t=*this;
-    c.n--;
+
+Counter Counter::operator--(int) {
+    Counter t = *this;
+    this->n--;
     return t;
 }
-void Counter::display(){
-    cout<<"counter number="<<n<<endl;
+
+void Counter::display() {
+    cout << "counter number=" << n << endl;
 }
-int main(int argc, char const *argv[])
-{
+
+int main(int argc, char const *argv[]) {
     Counter a;
     ++a;
     a.display();
@@ -44,6 +55,5 @@ int main(int argc, char const *argv[])
     a.display();
     a--;
     a.display();
-    system("pause");
     return 0;
 }
